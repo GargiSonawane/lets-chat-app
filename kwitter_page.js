dda99b02aaf2ba6bdf3f419cb87df611fb58fc06
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2021-02-25 18:45:38 -0500

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -33,7 +33,28 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+name=message_data['name'];
+message=message_data['message'];
+like = message_data['like'];
+name_with_tag = "<h4> " + name + "<img class='user_tick' src='m2.png'><h4>"
+message_with_tag="<h4 class='message_h4'>"+ message + "</h4>";
+like_button="<button class='btn btn-warning' id=" + firebase_message_id + " value="+like+ " onclick='updateLike(this.id)'>";
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>like: "+like+"</span></button><hr>";
+row= name_with_tag + message_with_tag + like_button + span_with_tag;
+document.getElementById("output").innerHTML += row;
 //End code
       } });  }); }
 getData();
+function updatelike(id_message){
+      button_id = id_message;
+      like = document.getElementById(button_id).value;
+      update_likes = Number(like)+1;
+      firebase.database().ref(room_name).child(id_message).update({
+            like:update_likes
+      });
+}
+function logout(){
+      localStorage.removeItem("username");
+      localStorage.removeItem("room_name");
+      window.location = "index.html";
+}
